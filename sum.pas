368405cd75661b9e5cb6d75eb0e8c6e917d1ae67
Hashan Dimuthu <91044348+HashanDimuthu@users.noreply.github.com>
2021-12-29 04:06:37 -0800

    Add files via upload

---
+++ sum.pas
@@ -0,0 +1,18 @@
+program sum(input,output);
+
+var     no1:integer;
+        no2:integer;
+        Total:integer;
+
+Begin
+        writeln('Enter your 1st number');
+        readln(no1);
+
+        writeln('Enter your 2nd number');
+        readln(no2);
+
+        Total:=no1+no2;
+
+        writeln('your Total is',' ',Total);
+        readln;
+end.
