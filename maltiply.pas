246b350895750b8248e8c6c08ed7735c2545cd41
Hashan Dimuthu <91044348+HashanDimuthu@users.noreply.github.com>
2021-12-29 21:06:54 -0800

    Add files via upload

---
+++ maltiply.pas
@@ -0,0 +1,18 @@
+program maltiply(input,output);
+
+var no1:integer;
+    no2:integer;
+    maltiply:integer;
+
+begin writeln('Enter Your 1 st Number');
+	  readln(no1);
+
+	  writeln('Enter Your 2 nd Number');
+	  readln(no2);
+
+	  matiply:=no1*no2;
+
+	  writeln('Your Maltiply is',' ',maltiply);
+	  readln;
+
+end.
