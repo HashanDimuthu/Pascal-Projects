f2c00cc840737c63e620ba07eb72c0949e9b8e3f
Hashan Dimuthu <91044348+HashanDimuthu@users.noreply.github.com>
2021-12-29 23:06:19 -0800

    Update result.pas

--- result.pas
+++ result.pas
@@ -2,6 +2,10 @@ program result(input,output);
 
 var     X: integer;
 
-Begin
-
+Begin   Writeln('Enter Your Marks');
+        real(X);
+        
+        if X >= 75 then
+        writeln('You are pass the exam');
+        readln;
 end.
