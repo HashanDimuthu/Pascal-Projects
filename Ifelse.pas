307bd690d5965c18f5a1f168eaaf2c5724f1caa6
Hashan Dimuthu <91044348+HashanDimuthu@users.noreply.github.com>
2021-12-29 22:33:29 -0800

    Update Ifelse.pas

--- Ifelse.pas
+++ Ifelse.pas
@@ -1,3 +1,14 @@
 program ifelse(input,output);
 
 var X: integer;
+
+Begin if X > 18 then
+      writeln('You are a adult');
+      readln;
+      
+      if X < 18 then 
+      writeln('You are a child');
+      readln;
+end.
+      
+    
